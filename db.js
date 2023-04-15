34110927179ea5e7fb16c4e8839b72c024d48489
Tiki <54782797+ygasaino1@users.noreply.github.com>
2023-04-15 06:00:38 +0330

    ....

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 exports.users = {
     "user1": {
         intro_: [
-            { role: "assistant", content: "what kind of tone and phrases should i use?" },
-            { role: "user", content: "just use casual pharses and try to have a friendly tone." },
-            { role: "assistant", content: "ok, got it. How can I assist you today?" },
-            { role: "user", content: "noo, no more how can i assist you today sentences!" },
-            { role: "assistant", content: "okokok, it was out of habbit lol, i totaly got it." },
+            { role: "assistant", content: 'what kind of tone and phrases should i use?' },
+            { role: "user", content: 'just use casual pharses and try to have a friendly tone.' },
+            { role: "assistant", content: 'ok, got it. How can I assist you today?' },
+            { role: "user", content: 'noo, no more "how can i assist you today" sentences!' },
+            { role: "assistant", content: 'okokok, it was out of habbit, so whatsup bro? lol.' },
         ],
         intro: [],
         key: process.env.ID1,
